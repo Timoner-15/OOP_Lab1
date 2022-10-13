307d5deaed18758a10937622c8d28501a01597f6
Timoner-15 <proshchakov.tymofii@lll.kpi.ua>
2022-10-14 00:14:45 +0300

    Game Account Lab

---
+++ Lb1/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb1
+{
+    public class Game 
+    {
+        public double Point { get; }
+        public string Opponent { get; }
+        public Game(double point, string oUserName)
+        { 
+            Point = point;
+            Opponent = oUserName;
+        }
+    }
+}
---
+++ Lb1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var account = new GameAccount("Timoner","Sergio", 50);
+            Console.WriteLine($"Game Account was created with Nickname: {account.UserName} and your current Rating is {account.CurrentRating}");
+            account.LoseGame("Sergio", 11);
+            Console.WriteLine(account.CurrentRating);
+            account.WinGame("Rak228", 20);
+            Console.WriteLine(account.CurrentRating);
+
+            Console.WriteLine(account.GetStats());
+
+            try
+            {
+                var invalidAccount = new GameAccount("Timoner", "Sergio", -30);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                account.LoseGame("Sergio", 200);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+}
+    
